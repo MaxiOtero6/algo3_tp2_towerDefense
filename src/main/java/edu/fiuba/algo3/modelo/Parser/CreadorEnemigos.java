cbc65adac3d54ba418c6b4d685c9c3b87f877687
Maximiliano Otero <maximilianotero7@gmail.com>
2023-06-03 14:24:02 -0300

    Cambiando crearEnemigos

--- src/main/java/edu/fiuba/algo3/modelo/Parser/CreadorEnemigos.java
+++ src/main/java/edu/fiuba/algo3/modelo/Parser/CreadorEnemigos.java
@@ -1,6 +1,7 @@
 package edu.fiuba.algo3.modelo.Parser;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 import com.fasterxml.jackson.databind.JsonNode;
@@ -10,39 +11,38 @@ import edu.fiuba.algo3.modelo.Enemigos.Enemigo;
 import edu.fiuba.algo3.modelo.Enemigos.Hormiga;
 
 public class CreadorEnemigos {
+
+    private final static JsonNode ROOT = ParserJSON.leerJSON(
+                                "src\\main\\resources\\json\\enemigos.json"
+                                        );
     
-    public static List<List<Enemigo>> crearEnemigos()
+    public static List<Enemigo> crearEnemigos(int numeroTurno)
     {
-        List<List<Enemigo>> enemigos = new ArrayList<>();
-        List<Enemigo> enemigosTurno;
+        List<Enemigo> enemigosTurno = new LinkedList<>();
 
         int cantidadHormigas;
         int cantidadAranias;
 
-        JsonNode root = ParserJSON.leerJSON(
-            "src\\main\\resources\\json\\enemigos.json"
-            );
-    
         try
         {
-            for (JsonNode turno : root)
+            for (JsonNode turno : ROOT)
             {
-                enemigosTurno = new ArrayList<>();
-                
-                cantidadHormigas = turno.get("enemigos").get("hormiga").asInt();
-                cantidadAranias = turno.get("enemigos").get("arana").asInt();
-                
-                for (int i = 0; i < cantidadHormigas; i++) {
-                    enemigosTurno.add(new Hormiga());
+                if ((turno.get("turno").asInt() - 1) == numeroTurno)
+                {    
+                    cantidadHormigas = turno.get("enemigos").get("hormiga").asInt();
+                    cantidadAranias = turno.get("enemigos").get("arana").asInt();
+                    
+                    for (int i = 0; i < cantidadHormigas; i++) {
+                            enemigosTurno.add(new Hormiga());
+                        }
+                        
+                    for (int i = 0; i < cantidadAranias; i++) {
+                            enemigosTurno.add(new Arania());
+                        }
                 }
-                
-                for (int i = 0; i < cantidadAranias; i++) {
-                    enemigosTurno.add(new Arania());
-                }
-                enemigos.add(enemigosTurno);
             }
 
-            return enemigos;
+            return enemigosTurno;
         }
         catch (NullPointerException e)
         {
