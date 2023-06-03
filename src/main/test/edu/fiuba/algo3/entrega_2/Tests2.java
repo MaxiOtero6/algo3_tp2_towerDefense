21fcbaa5a340144537767df1f800930c2015d9c1
Maximiliano Otero <maximilianotero7@gmail.com>
2023-06-03 15:08:20 -0300

    Esqueleto casos de uso entrega 2

---
+++ src/main/test/edu/fiuba/algo3/entrega_2/Tests2.java
@@ -0,0 +1,54 @@
+package edu.fiuba.algo3.entrega_2;
+
+import org.junit.jupiter.api.Test;
+
+public class Tests2 {
+    
+    @Test
+    public void test13ElFormatoDelJsonEnemigosEsValido()
+    {
+
+    }
+
+    @Test
+    public void test14ElFormatoDelJsonMapaEsValido()
+    {
+
+    }
+
+    @Test
+    public void test15LaConversionDelJsonEnemigosEsLaIndicada()
+    {
+
+    }
+
+    @Test
+    public void test16LaConversionDelJsonMapaEsLaIndicada()
+    {
+
+    }
+
+    @Test
+    public void test17ElJuegoSeCreaAcordeALosJson()
+    {
+
+    }
+
+    @Test
+    public void test18ElJugadorGanaLaPartidaSimulada()
+    {
+
+    }
+
+    @Test
+    public void test19ElJugadorPierdeLaPartidaSimulada()
+    {
+
+    }
+
+    @Test
+    public void test20VerificacionConsoleLog()
+    {
+
+    }
+}
