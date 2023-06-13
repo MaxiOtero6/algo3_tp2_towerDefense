013456d5169ea763adef557a8e86db57a97533e7
Tomas Pla Hozikian <tpla@fi.uba.ar>
2023-06-12 22:40:38 -0300

    javaFX intro
    Co-Authored-By: ColoOviedo <105893846+ColoOviedo@users.noreply.github.com>
    Co-Authored-By: Lucasfz25 <91162762+Lucasfz25@users.noreply.github.com>
    Co-Authored-By: Maximiliano Otero <106620426+MNOtero@users.noreply.github.com>
    Co-Authored-By: Joaquin Velurtas <jvelurtas@fi.uba.ar>

--- src/main/java/edu/fiuba/algo3/App.java
+++ src/main/java/edu/fiuba/algo3/App.java
@@ -3,8 +3,11 @@ package edu.fiuba.algo3;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+
 
 /**
  * JavaFX App
@@ -12,18 +15,39 @@ import javafx.stage.Stage;
 public class App extends Application {
 
     @Override
-    public void start(Stage stage) {
-        var javaVersion = SystemInfo.javaVersion();
-        var javafxVersion = SystemInfo.javafxVersion();
+    public void start(Stage primaryStage) {
+        TextField textField = new TextField();
+        Button okButton = new Button("OK");
+        Button iniButton = new Button("Iniciar Partida");
+        Label validationLabel = new Label();
 
-        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        var scene = new Scene(new StackPane(label), 640, 480);
-        stage.setScene(scene);
-        stage.show();
+        iniButton.setVisible(false);
+
+        okButton.setOnAction(event -> {
+            String inputText = textField.getText();
+            if (inputText.length() >= 6) {
+                validationLabel.setText("Nombre valido: " + inputText);
+                textField.setDisable(true);
+                okButton.setDisable(true);
+                iniButton.setVisible(true);
+            } else {
+                validationLabel.setText("Ingrese un nombre de al menos 6 caracteres");
+            }
+        });
+
+        iniButton.setOnAction(event -> {
+            validationLabel.setText("Partida iniciada");
+        });
+
+        VBox root = new VBox(textField, okButton, validationLabel, iniButton);
+        root.setSpacing(10);
+
+        Scene scene = new Scene(root, 300, 200);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
-
 }
