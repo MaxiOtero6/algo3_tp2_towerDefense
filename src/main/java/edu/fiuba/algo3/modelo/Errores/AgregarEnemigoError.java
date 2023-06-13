394987aed810e3732dd703b2e556fb2a8208e687
Maximiliano Otero <maximilianotero7@gmail.com>
2023-06-13 19:39:25 -0300

    Borrando excepcion sin uso

--- src/main/java/edu/fiuba/algo3/modelo/Errores/AgregarEnemigoError.java
+++
@@ -1,8 +0,0 @@
-package edu.fiuba.algo3.modelo.Errores;
-
-public class AgregarEnemigoError extends RuntimeException{
-    public AgregarEnemigoError(){
-        super("El jugador no tiene los creditos necesarios para construir");
-    }
-    
-}
