7b20d4ea77f692a567c32f431f953c590f26906e
Tomas Pla Hozikian <tpla@fi.uba.ar>
2023-06-18 14:08:59 -0300

    fix2 error terreno erroneo de defensa
    Co-Authored-By: ColoOviedo <105893846+ColoOviedo@users.noreply.github.com>
    Co-Authored-By: Lucasfz25 <91162762+Lucasfz25@users.noreply.github.com>
    Co-Authored-By: Maximiliano Otero <106620067+MaxiOtero6@users.noreply.github.com>
    Co-Authored-By: Joaquin Velurtas <jvelurtas@fi.uba.ar>

--- src/main/java/edu/fiuba/algo3/modelo/Errores/DefensaEnTerrenoErroneoError.java
+++ src/main/java/edu/fiuba/algo3/modelo/Errores/DefensaEnTerrenoErroneoError.java
@@ -8,21 +8,21 @@ import edu.fiuba.algo3.modelo.Parcelas.Parcela;
 import edu.fiuba.algo3.modelo.Parcelas.Pasarela;
 import edu.fiuba.algo3.modelo.Parcelas.Tierra;
 
-public class DefensaEnTerrenoErroneoError extends RuntimeException {
-    public DefensaEnTerrenoErroneoError() {
+public class DefensaEnTerrenoErroneoError extends RuntimeException{
+    public DefensaEnTerrenoErroneoError(){
         super("No se puede construir este tipo de defensa en esta parcela!");
     }
 
-    public static void comprobarTerreno(Parcela parcela, Defensa defensa) {
-        if (parcela instanceof Tierra) {
-            if (defensa instanceof TrampaArenosa) {
-                throw new DefensaEnTerrenoErroneoError();
-            }
-        } else if (parcela instanceof Pasarela) {
-            if (defensa instanceof TorreBlanca || defensa instanceof TorrePlateada) {
-                throw new DefensaEnTerrenoErroneoError();
-            }
+    public static void comprobarTerreno(Parcela parcela, Defensa defensa)
+    {
+        if (parcela instanceof Tierra)
+        {
+            if (defensa instanceof TrampaArenosa) {throw new DefensaEnTerrenoErroneoError();}
+        }
+
+        else if (parcela instanceof Pasarela)
+        {
+            if (defensa instanceof TorreBlanca || defensa instanceof TorrePlateada) {throw new DefensaEnTerrenoErroneoError();}
         }
     }
 }
-
