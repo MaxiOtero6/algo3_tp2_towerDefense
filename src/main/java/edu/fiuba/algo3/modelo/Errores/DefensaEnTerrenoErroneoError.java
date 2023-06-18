5ab4ae5b5f7bc87af0d046fa228dbd87e2f578b2
Tomas Pla Hozikian <tpla@fi.uba.ar>
2023-06-18 13:25:06 -0300

    fix error terreno erroneo de defensa
    Co-Authored-By: ColoOviedo <105893846+ColoOviedo@users.noreply.github.com>
    Co-Authored-By: Lucasfz25 <91162762+Lucasfz25@users.noreply.github.com>
    Co-Authored-By: Maximiliano Otero <106620067+MaxiOtero6@users.noreply.github.com>
    Co-Authored-By: Joaquin Velurtas <jvelurtas@fi.uba.ar>

--- src/main/java/edu/fiuba/algo3/modelo/Errores/DefensaEnTerrenoErroneoError.java
+++ src/main/java/edu/fiuba/algo3/modelo/Errores/DefensaEnTerrenoErroneoError.java
@@ -2,26 +2,27 @@ package edu.fiuba.algo3.modelo.Errores;
 
 import edu.fiuba.algo3.modelo.Defensas.Defensa;
 import edu.fiuba.algo3.modelo.Defensas.Torres.TorreBlanca;
+import edu.fiuba.algo3.modelo.Defensas.Torres.TorrePlateada;
 import edu.fiuba.algo3.modelo.Defensas.Trampas.TrampaArenosa;
 import edu.fiuba.algo3.modelo.Parcelas.Parcela;
 import edu.fiuba.algo3.modelo.Parcelas.Pasarela;
 import edu.fiuba.algo3.modelo.Parcelas.Tierra;
 
-public class DefensaEnTerrenoErroneoError extends RuntimeException{
-    public DefensaEnTerrenoErroneoError(){
+public class DefensaEnTerrenoErroneoError extends RuntimeException {
+    public DefensaEnTerrenoErroneoError() {
         super("No se puede construir este tipo de defensa en esta parcela!");
     }
 
-    public static void comprobarTerreno(Parcela parcela, Defensa defensa)
-    {
-        if (parcela instanceof Tierra)
-        {
-            if (defensa instanceof TrampaArenosa) {throw new DefensaEnTerrenoErroneoError();}
-        }
-
-        else if (parcela instanceof Pasarela)
-        {
-            if (defensa instanceof TorreBlanca) {throw new DefensaEnTerrenoErroneoError();}
+    public static void comprobarTerreno(Parcela parcela, Defensa defensa) {
+        if (parcela instanceof Tierra) {
+            if (defensa instanceof TrampaArenosa) {
+                throw new DefensaEnTerrenoErroneoError();
+            }
+        } else if (parcela instanceof Pasarela) {
+            if (defensa instanceof TorreBlanca || defensa instanceof TorrePlateada) {
+                throw new DefensaEnTerrenoErroneoError();
+            }
         }
     }
 }
+
