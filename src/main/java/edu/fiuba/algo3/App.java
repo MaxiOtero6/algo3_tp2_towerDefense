22008cb3765c52951d016d591b9059eb62f963c9
Tomas Pla Hozikian <tpla@fi.uba.ar>
2023-06-19 13:28:05 -0300

    boton skip turno
    Co-Authored-By: ColoOviedo <105893846+ColoOviedo@users.noreply.github.com>
    Co-Authored-By: Lucasfz25 <91162762+Lucasfz25@users.noreply.github.com>
    Co-Authored-By: Maximiliano Otero <106620426+MNOtero@users.noreply.github.com>
    Co-Authored-By: Joaquin Velurtas <jvelurtas@fi.uba.ar>

--- src/main/java/edu/fiuba/algo3/App.java
+++ src/main/java/edu/fiuba/algo3/App.java
@@ -2,9 +2,11 @@ package edu.fiuba.algo3;
 
 import edu.fiuba.algo3.vista.IntroMenu;
 import javafx.application.Application;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 
 
 /**
@@ -16,10 +18,11 @@ public class App extends Application {
     public void start(Stage primaryStage) {
         IntroMenu intro = new IntroMenu();
         VBox root = intro.crearUI(primaryStage);
-
+        root.setStyle("-fx-background-color: grey;");
         Scene scene = new Scene(root, 300, 300);
         primaryStage.setScene(scene);
         primaryStage.show();
+
     }
 
     public static void main(String[] args) {
